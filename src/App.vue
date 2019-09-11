3ec9d74d49d7b607af85627b9ddd20ad55f4d0a0
curry <907784820@qq.com>
2019-09-11 23:36:25 +0800

    登录提交

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <div id="app">
+    <router-link to='/home'>这是home</router-link>
+    <router-link to='/login'>这是login</router-link>
+
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+export default {
+  
+}
+</script>
+<style>
+
+</style>
+
